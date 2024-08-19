066d406b5563b1eacd9b6c2ff3beff59b01d987c
satokyoko <202406ap01_sato@x-ide.sakura.ne.jp>
2024-08-19 15:35:19 +0900

    売主物件_テスト仕様書バグNo.264（築年の数値0）

--- SRC/class/sell/model.php
+++ SRC/class/sell/model.php
@@ -85,7 +85,7 @@ function fnSqlSellList($flg, $param)
 function fnSqlSellEdit($sellNo)
 {
     $select = "SELECT IF(SEARCHDT > '0000-00-00',DATE_FORMAT(SEARCHDT,'%Y/%m/%d'),''),ARTICLE,ADDRESS,STATION,IF(FOOT >= 0,FOOT,''),";
-    $select .= "IF(YEARS > 0,YEARS,''),IF(FLOOR >= 0,FLOOR,''),IF(AREA >= 0,AREA,''),SELLER,IF(PRICE >= 0,PRICE,''),NOTE";
+    $select .= "IF(YEARS >= 0,YEARS,''),IF(FLOOR >= 0,FLOOR,''),IF(AREA >= 0,AREA,''),SELLER,IF(PRICE >= 0,PRICE,''),NOTE";
     $from = " FROM TBLSELL";
     $where = " WHERE DEL = 1";
     $where .= " AND SELLNO = $sellNo";
