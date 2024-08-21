e2afd4232dcc2852c1209fab7797c310320ee08f
satokyoko <202406ap01_sato@x-ide.sakura.ne.jp>
2024-08-21 14:51:25 +0900

    物件管理_テスト仕様書バグNo.366（XSS）

--- SRC/js/guide.js
+++ SRC/js/guide.js
@@ -63,8 +63,8 @@ function fnGuideMove(muki, articleNo) {
 		var obj = $("#s" + articleNo);
 
 		// 名称の取得
-		var articleName = $(obj.children()[0]).text();
-		var articleRoom = $(obj.children()[1]).text();
+		var articleName = $(obj.children()[0]).html();
+		var articleRoom = $(obj.children()[1]).html();
 		var articleKey = $(obj.children()[2]).html();
 
 		// 登録リストに保存(右側に移動)
